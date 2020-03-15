5f08f9f929aaffa8fa2b7fa725ec9332c19e261d
黄义诚 <huangdaren1997@gmail.com>
2020-03-15 11:45:39 +0800

    init

---
+++ jdk.jdeps/com/sun/tools/javap/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javap.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "11.0.6+10-post-Ubuntu-1ubuntu118.04.1" },
+            { "jdk", "11.0.6" },
+            { "release", "11.0.6" },
+        };
+    }
+}
